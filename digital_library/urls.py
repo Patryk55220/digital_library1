934ff073f41d47f30626d40a5b7b116952418358
Patryk55220 <malimpa7243_aehit@students.vizja.pl>
2025-05-24 20:44:37 +0200

    Update urls.py

--- digital_library/urls.py
+++ digital_library/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("library.urls", namespace="library")),
+    path("admin/",    admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("",          include("library.urls", namespace="library")),
 ]
